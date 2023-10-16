4cc6ab2dd6ad768e74b58f6c8bf537e44091d6de
Andoniank <andoniank@gmail.com>
2023-10-16 00:09:23 -0700

    Finish index page and start Show page

--- frontend/src/components/CampsiteShow/CampsiteShow.jsx
+++ frontend/src/components/CampsiteShow/CampsiteShow.jsx
@@ -3,6 +3,8 @@ import './CampsiteShow.css'
 import { useEffect } from 'react'
 import { fetchCampsite } from '../../store/campsites'
 import { useParams } from 'react-router-dom'
+import campsitedemo2 from '../../assets/campsitedemo2.jpg'
+import heart from '../../assets/heart.png'
 
 const CampsiteShow = () => {
     const {campsiteId} = useParams()
@@ -16,8 +18,26 @@ const CampsiteShow = () => {
     if(!campsite) return null
 
     return (
-        <div>
-            <h1>{campsite.name}</h1>
+        <div className='campsite-show'>
+            <div className='show-header'>
+                <h1 className='show-title'>{campsite.name}</h1>
+                <p className='show-location'>95% · 323 reviews · {campsite.location}</p>
+                <button className='save'><img className='save-icon' src={heart} alt="" />Save</button>
+            </div>
+            <div className='show-img-div'>
+                <img className='show-img' src={campsitedemo2} alt="" />
+            </div>
+            <div className='show-details'>
+                <h2>{campsite.acres} acres hosted by John Doe</h2>
+                <p className='includes'>{campsite.lodging ? 'Lodging ·' : ''} {campsite.rv? 'RVs ·' : ''} {campsite.tents ? 'Tents' : ''}</p>
+                <p className='show-description'>{campsite.description}</p>
+            </div>
+            <div className='activities-features'>
+                <h2>Activities</h2>
+                    <ul>
+                        <li>biking</li>
+                    </ul>
+            </div>
         </div>
     )
 }
--- frontend/src/components/CampsitesIndex/CampsiteIndex.jsx
+++ frontend/src/components/CampsitesIndex/CampsiteIndex.jsx
@@ -1,37 +1,70 @@
 import {useDispatch, useSelector} from 'react-redux'
 import './CampsiteIndex.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { fetchCampsites } from '../../store/campsites'
+import campsitedemo from '../../assets/campsitedemo.webp'
+import { NavLink } from 'react-router-dom'
+import lodging from '../../assets/lodging.png'
+import rv from '../../assets/rv.png'
+import tent from '../../assets/tent.png'
 
 const CampsiteIndex = () => {
     const campsites = useSelector(state => state.campsites)
     const campsitesArray = Object.values(campsites)
     const dispatch = useDispatch()
+    const [filter, setFilter] = useState(null)
 
     useEffect(() => {
         dispatch(fetchCampsites())
     }, [dispatch])
 
+    let filteredCampsites = campsitesArray
+    if (filter) {
+        filteredCampsites = campsitesArray.filter(campsite => campsite[filter])
+    }
+
+
     return (
-        <div className='campsite-index'>
-            <h1>Campsite Index</h1>
-            {campsitesArray.map((campsite) => {
-                return (
-                    <div>
-                        <ul>
-                            <li>Name: {campsite.name}</li>
-                            <li>Location: {campsite.location}</li>
-                            <li>Acres: {campsite.acres}</li>
-                            <li>Max Guests: {campsite.max_guests}</li>
-                            <li>Daily Rates: {campsite.daily_rates}</li>
-                            <li>Lodging: {campsite.lodging}</li>
-                            <li>RV: {campsite.rv}</li>
-                            <li>Tents: {campsite.tents}</li>
-                        </ul>
-                    </div>
-                )
-            })}
-        </div>
+        <>
+            <div className='filters'>
+                <button onClick={() => setFilter(filter === 'tents' ? null : 'tents')}
+                className={`filters-button ${filter === 'tents' ? 'active-filter' : ''}`}>
+                    <img className='filter-icon-tent' src={tent} alt="" />Campsites
+                </button>
+                <button onClick={() => setFilter(filter === 'lodging' ? null : 'lodging')}
+                className={`filters-button ${filter === 'lodging' ? 'active-filter' : ''}`}>
+                    <img className='filter-icon-lodging' src={lodging} alt="" />Lodging
+                </button>
+                <button onClick={() => setFilter(filter === 'rv' ? null : 'rv')}
+                className={`filters-button ${filter === 'rv' ? 'active-filter' : ''}`}>
+                    <img className='filter-icon-rv' src={rv} alt="" />Rvs
+                </button>
+            </div>
+            <div className='campsite-index'>
+                {filteredCampsites.map((campsite) => {
+                    return (
+                        <NavLink to={`/campsites/${campsite.id}`} className='campsite-link'>
+                            <div className='campsite'>
+                                <img className='campsite-img' src={campsitedemo} alt="" />
+                                <h3 className='campsite-name'>{campsite.name}</h3>
+                                <div className='campsite-details'> 
+                                    <div className='campsite-L1'>
+                                    <p>Sleeps {campsite.maxGuests} <span className="centered-dot">·</span> {campsite.lodging ? 'Lodging,' : ''} {campsite.rv? 'RVs,' : ''} {campsite.tents ? 'Tents' : ''}</p> 
+                                    </div>
+                                    <div className='campsite-L2'>
+                                    <p>{campsite.acres} acres <span className="centered-dot">·</span> {campsite.location}</p>
+                                    </div>
+                                    <div className='campsite-L3'>
+                                        <p>from <span className='daily-rates'>${campsite.dailyRates}</span><span className='night'>/night</span></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </NavLink>
+                    )
+                })}
+            </div>
+        </>
+
     )
 }
 
