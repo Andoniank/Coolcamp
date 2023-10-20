48d4bffc02cd32b505ad2fa2315a43e77c257f03
Andoniank <andoniank@gmail.com>
2023-10-20 00:54:16 -0700

    Finish showpage

--- frontend/src/components/CampsiteShow/CampsiteShow.jsx
+++ frontend/src/components/CampsiteShow/CampsiteShow.jsx
@@ -4,8 +4,10 @@ import { useEffect } from 'react'
 import { fetchCampsite } from '../../store/campsites'
 import { useParams } from 'react-router-dom'
 import Reservations from '../Reservations/Reservations'
-import campsitedemo2 from '../../assets/campsitedemo2.jpg'
 import heart from '../../assets/heart.png'
+import * as activitiesIcons from '../../assets/Showpage/activities'
+import * as naturalFeaturesIcons from '../../assets/Showpage/naturalfeatures'
+import * as amenitiesIcons from '../../assets/Showpage/amenities'
 
 const CampsiteShow = () => {
     const {campsiteId} = useParams()
@@ -19,6 +21,43 @@ const CampsiteShow = () => {
     if(!campsite) return null
     
 
+    const activityIcon = {
+        'Biking' : activitiesIcons.biking,
+        'Hiking': activitiesIcons.hiking,
+        "Wildlife watching": activitiesIcons.wildlifeWatching,
+        "Climbing": activitiesIcons.climbing,
+        "Fishing": activitiesIcons.fishing,
+        "Off-roading (OHV)": activitiesIcons.offRoading,
+        "Swimming": activitiesIcons.swimming,
+        "Horseback riding": activitiesIcons.horsebackRiding,
+        "Skiing": activitiesIcons.skiing
+    }
+
+    const naturalFeaturesIcon = {
+        'Mountains': naturalFeaturesIcons.mountain,
+        'River': naturalFeaturesIcons.river,
+        'Forest': naturalFeaturesIcons.forest,
+        'Desert': naturalFeaturesIcons.desert,
+        'Canyon': naturalFeaturesIcons.canyon,
+        'Lake': naturalFeaturesIcons.lake,
+        'Beach': naturalFeaturesIcons.beach,
+        "Ocean": naturalFeaturesIcons.ocean,
+        "Field": naturalFeaturesIcons.feild,
+        "Swamp": naturalFeaturesIcons.swamp
+    }
+
+    const amenitiesIcon = {
+        'Campfires allowed': amenitiesIcons.campfire,
+        'Toilets Available': amenitiesIcons.toilet,
+        'Wifi available': amenitiesIcons.wifi,
+        'Pets allowed': amenitiesIcons.pets,
+        'Cooking equipment present': amenitiesIcons.cooking,
+        'Bins available': amenitiesIcons.trash,
+        'Showers available': amenitiesIcons.shower
+    }
+
+
+
     return (
         <div className='campsite-show'>
             <div className='show-header'>
@@ -29,22 +68,44 @@ const CampsiteShow = () => {
             <div className='show-img-div'>
                 <img className='show-img' src={campsite.photoUrl} alt="" />
             </div>
-            <div className='details-reservaitons-container'>
+            <div className='container'>
+
+            <div className='left-container'>
                 <div className='show-details'>
-                    <h2>{campsite.acres} acres hosted by John Doe</h2>
-                    <p className='includes'>{campsite.lodging ? 'Lodging ·' : ''} {campsite.rv? 'RVs ·' : ''} {campsite.tents ? 'Tents' : ''}</p>
+                    <h2 className='show-acres'>{campsite.acres} acres hosted by John Doe</h2>
+                    <p className='includes'>{campsite.lodging ? 'Lodging ·' : ''} {campsite.rv? 'RVs ·' : ''} {campsite.tents ? 'Tents' : ''} · Sleeps {campsite.maxGuests}</p>
                     <p className='show-description'>{campsite.description}</p>
                 </div>
-                <div className='show-reservations'>
-                    <Reservations />
+                <div className='activities-features'>
+                    <div className='activities'>
+                        <h2 className='activities-header'>Activities</h2>
+                        {campsite.activities && campsite.activities.map((activity) => {
+                            return (
+                                <p className='activity'><img className='activity-icon' src={activityIcon[activity]} alt=''></img> {activity}</p>
+                            )
+                        })}
+                    </div>
+                    <div className='natural-features'>
+                        <h2 className='natural-features-header'>Natural Features</h2>
+                        {campsite.naturalFeatures && campsite.naturalFeatures.map((naturalFeature) => {
+                            return(
+                                <p className='natural-feature'><img className='natural-features-icon' src={naturalFeaturesIcon[naturalFeature]} alt="" /> {naturalFeature}</p>
+                            )
+                            })}
+                    </div>
+                </div>
+                <div className='amenities'>
+                    <h2 className='amenities-header'>What this site offers</h2>
+                    {campsite.amenities && campsite.amenities.map((amenity) => {
+                        return (
+                            <p className='amenity'> <img className='amenities-icon' src={amenitiesIcon[amenity]} alt="" /> {amenity}</p>
+                        )
+                    })}
                 </div>
             </div>
-                
-            <div className='activities-features'>
-                <h2>Activities</h2>
-                    <ul>
-                        <li>biking</li>
-                    </ul>
+            <div className='show-reservations'>
+                <Reservations />
+            </div>
             </div>
         </div>
     )
--- frontend/src/components/Reservations/Reservations.jsx
+++ frontend/src/components/Reservations/Reservations.jsx
@@ -9,13 +9,19 @@ import './Reservations.css'
 const Reservations = () => {
     const {campsiteId} = useParams()
     const sessionUser = useSelector(state => state.session.user)
-    
+    const campsite = useSelector(state => (state.campsites[campsiteId]))
     const [startDate, setStartDate] = useState()
     const [endDate, setEndDate] = useState()
     const [showModal, setShowModal] = useState(false)
     const dispatch = useDispatch()
     
+    const ratesCalc = () => {
+        return campsite.dailyRates * campsite.maxGuests
+    }
 
+    const ratesPlusService = () => {
+        return ratesCalc() + 25
+    }
 
     const handleSubmit = (e) => {
         e.preventDefault()
@@ -30,8 +36,9 @@ const Reservations = () => {
 
     return (
         <>
-            <div>
-                <h3>Reserve this Campsite Today!</h3>
+            <div className="reservation-display">
+                <h3 className="rates">${ratesPlusService()}<p className="total"> total</p></h3>
+                <p className="reservation-max-guests">for {campsite.maxGuests} guests</p>
                 <form className='reservation-form' onSubmit={handleSubmit}>
                     
                         <input className="reservation-input" type="date" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
@@ -41,6 +48,10 @@ const Reservations = () => {
                         <br />
                     <button className="reservation-submit">Reserve</button>
                 </form>
+                <p className='charged-yet'>you won't be charged yet</p>
+                <div className="total-with-guests"><p>{campsite.maxGuests} guests</p><p>${ratesCalc()}.00</p></div>
+                <div className="service"><p>Service Fee</p><p>$25.00</p></div>
+                <div className="total"><p>Total</p><p>${ratesPlusService()}.00</p></div>
                 {showModal && (
                 <Modal onClose={() => setShowModal(false)}>
                     <LoginFormPage />
